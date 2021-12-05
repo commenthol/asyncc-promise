02e47ba8fa4c726414ac55ce29109dcd76c79eaa
commenthol <commenthol@gmail.com>
2021-12-05 09:45:11 +0100

    docs: jsdoc is broken

--- src/eachLimit.js
+++ src/eachLimit.js
@@ -1,56 +1,56 @@
 import { EachLimit } from './intern/Parallel'
 
 /**
-* Run `items` on async `task` promise in parallel limited to `limit` running in
-* parallel.
-*
-* Does not stop parallel execution on errors. *All tasks get executed.*
-*
-* @name eachLimit
-* @memberOf module:parallel
-* @static
-* @method
-* @param {Number} limit - number of tasks running in parallel
-* @param {Array<any>} items - Array of items
-* @param {Function} task - iterator function of type `(item: any, index: Number) => Promise`
-* @param {Object} [options]
-* @param {Number} [options.timeout] - timeout in ms which throws `AsynccError` in case that `tasks` are still running
-* @param {Boolean} [options.bail] - bail-out on first error
-* @return {Promise} on resolve `.then(results: Array<any> => {})` and
-* on reject `.catch(error: AsynccError => {})` where `error` is the first thrown
-* error containing the properties:
-* - `errors: Array<Error>` list of errors
-* - `errpos: Array<Number>` gives the positions of errors in order as they occur.
-* - `results: Array<Any>` returns the successfull results or undefined
-*
-* @example <caption>without errors</caption>
-* eachLimit(2, [1, 2, 3, 4],
-*  (item, index) => (
-*    new Promise((resolve, reject) => {
-*      resolve(item + index)
-*    }))
-* )
-* .then((results) => {
-*   console.log(results)
-*   //> [1, 3, 5, 7]
-* })
-* @example <caption>with errors</caption>
-* eachLimit(2, [1, 2, 3, 4],
-*  (item, index) => (
-*    new Promise((resolve, reject) => {
-*      if (index % 2) resolve(item + index)
-*      else reject(new TypeError('error'))
-*  }))
-* )
-* .catch((err) => { //
-*  console.log(err)
-*  //> { TypeError: error
-*  //>   errors: [[Circular], null, TypeError: error, null],
-*  //>   errpos: [0, 2],
-*  //>   results: [undefined, 3, undefined, 7]
-*  //> }
-* })
-*/
+ * Run `items` on async `task` promise in parallel limited to `limit` running in
+ * parallel.
+ *
+ * Does not stop parallel execution on errors. *All tasks get executed.*
+ *
+ * @name eachLimit
+ * @memberOf module:parallel
+ * @static
+ * @method
+ * @param {Number} limit - number of tasks running in parallel
+ * @param {Array<any>} items - Array of items
+ * @param {Function} task - iterator function of type `(item: any, index: Number) => Promise`
+ * @param {Object} [options]
+ * @param {Number} [options.timeout] - timeout in ms which throws `AsynccError` in case that `tasks` are still running
+ * @param {Boolean} [options.bail] - bail-out on first error
+ * @return {Promise} on resolve `.then(results: Array<any> => {})` and
+ * on reject `.catch(error: AsynccError => {})` where `error` is the first thrown
+ * error containing the properties:
+ * - `errors: Array<Error>` list of errors
+ * - `errpos: Array<Number>` gives the positions of errors in order as they occur.
+ * - `results: Array<Any>` returns the successfull results or undefined
+ *
+ * @example <caption>without errors</caption>
+ * eachLimit(2, [1, 2, 3, 4],
+ *  (item, index) => (
+ *    new Promise((resolve, reject) => {
+ *      resolve(item + index)
+ *    }))
+ * )
+ * .then((results) => {
+ *   console.log(results)
+ *   //> [1, 3, 5, 7]
+ * })
+ * @example <caption>with errors</caption>
+ * eachLimit(2, [1, 2, 3, 4],
+ *  (item, index) => (
+ *    new Promise((resolve, reject) => {
+ *      if (index % 2) resolve(item + index)
+ *      else reject(new TypeError('error'))
+ *  }))
+ * )
+ * .catch((err) => { //
+ *  console.log(err)
+ *  //> { TypeError: error
+ *  //>   errors: [[Circular], null, TypeError: error, null],
+ *  //>   errpos: [0, 2],
+ *  //>   results: [undefined, 3, undefined, 7]
+ *  //> }
+ * })
+ */
 export default function eachLimit (limit, items, task, options) {
   return new Promise((resolve, reject) => {
     new EachLimit(limit, items, task, options, resolve, reject)
--- src/parallelLimit.js
+++ src/parallelLimit.js
@@ -1,58 +1,58 @@
 import { ParallelLimit } from './intern/Parallel'
 
 /**
-* Run `tasks` returning Promises in parallel limited to `limit` parallel
-* running tasks.
-*
-* Consider using this function in case of opening a huge amout of parallel IO
-* like filehandlers or sockets. Remember those resources are limited!
-*
-* Does not stop parallel execution on errors. *All tasks get executed.*
-* `then()` gets called after the longest running task finishes.
-*
-* If bail-out on first error is desired, consider set `{bail: true}` as option.
-*
-* @name parallelLimit
-* @memberOf module:parallel
-* @static
-* @method
-* @param {Number} limit - number of tasks running in parallel
-* @param {Array<Function>} tasks - Array of functions of type `() => Promise`
-* @param {Object} [options]
-* @param {Number} [options.timeout] - timeout in ms which throws `AsynccError` in case that `tasks` are still running
-* @param {Boolean} [options.bail] - bail-out on first error
-* @return {Promise} on resolve `.then(results: Array<any> => {})` and
-* on reject `.catch(error: AsynccError => {})` where `error` is the first thrown
-* error containing the properties:
-* - `errors: Array<Error>` list of errors
-* - `errpos: Array<Number>` gives the positions of errors in order as they occur.
-* - `results: Array<Any>` returns the successfull results or undefined
-*
-* @example <caption>without errors - runs 2 tasks in parallel</caption>
-* parallelLimit(2, [
-*   () => Promise.resolve(1),
-*   () => Promise.resolve(3),
-*   () => Promise.resolve(5)
-* ]).then((results) => {
-*   console.log(results)
-*   //> [1, 3, 5]
-* })
-*
-* @example <caption>with errors and timeout = 100ms</caption>
-* parallelLimit(2, [
-*   () => Promise.reject(new Error(1)),
-*   () => Promise.resolve(3),
-*   () => Promise.reject(new Error(5))
-* ], {timeout: 100}).then((result) => { // won't reach here
-* }).catch((err) => {
-*   console.log(err)
-*   //> { AsynccError:
-*   //>   errors: [Error: 1, null, Error: 5],
-*   //>   errpos: [0, 2],
-*   //>   results: [undefined, 3, undefined]
-*   //> }
-* })
-*/
+ * Run `tasks` returning Promises in parallel limited to `limit` parallel
+ * running tasks.
+ *
+ * Consider using this function in case of opening a huge amout of parallel IO
+ * like filehandlers or sockets. Remember those resources are limited!
+ *
+ * Does not stop parallel execution on errors. *All tasks get executed.*
+ * `then()` gets called after the longest running task finishes.
+ *
+ * If bail-out on first error is desired, consider set `{bail: true}` as option.
+ *
+ * @name parallelLimit
+ * @memberOf module:parallel
+ * @static
+ * @method
+ * @param {Number} limit - number of tasks running in parallel
+ * @param {Array<Function>} tasks - Array of functions of type `() => Promise`
+ * @param {Object} [options]
+ * @param {Number} [options.timeout] - timeout in ms which throws `AsynccError` in case that `tasks` are still running
+ * @param {Boolean} [options.bail] - bail-out on first error
+ * @return {Promise} on resolve `.then(results: Array<any> => {})` and
+ * on reject `.catch(error: AsynccError => {})` where `error` is the first thrown
+ * error containing the properties:
+ * - `errors: Array<Error>` list of errors
+ * - `errpos: Array<Number>` gives the positions of errors in order as they occur.
+ * - `results: Array<Any>` returns the successfull results or undefined
+ *
+ * @example <caption>without errors - runs 2 tasks in parallel</caption>
+ * parallelLimit(2, [
+ *   () => Promise.resolve(1),
+ *   () => Promise.resolve(3),
+ *   () => Promise.resolve(5)
+ * ]).then((results) => {
+ *   console.log(results)
+ *   //> [1, 3, 5]
+ * })
+ *
+ * @example <caption>with errors and timeout = 100ms</caption>
+ * parallelLimit(2, [
+ *   () => Promise.reject(new Error(1)),
+ *   () => Promise.resolve(3),
+ *   () => Promise.reject(new Error(5))
+ * ], {timeout: 100}).then((result) => { // won't reach here
+ * }).catch((err) => {
+ *   console.log(err)
+ *   //> { AsynccError:
+ *   //>   errors: [Error: 1, null, Error: 5],
+ *   //>   errpos: [0, 2],
+ *   //>   results: [undefined, 3, undefined]
+ *   //> }
+ * })
+ */
 export default function parallelLimit (limit, tasks, options) {
   return new Promise((resolve, reject) => {
     new ParallelLimit(limit, tasks, options, resolve, reject)
